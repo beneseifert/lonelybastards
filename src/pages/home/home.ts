82c38ecd15e6fb4e0ad7ae8ba07cc138fb9af5e8
Benedikt Seifert <bene.seifert@gmail.com>
2017-03-25 11:37:39 +0100

    change maps Api Key

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,14 +2,19 @@
 
 import { NavController } from 'ionic-angular';
 
+import { Config } from '../../config/config';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  public mapsUrl = "https://maps.googleapis.com/maps/api/staticmap?center=Arabellastraße+23,Munich,Germany&markers=color:red%7CArabellastraße+23,Munich,Germany&zoom=16&size=500x400&key=";
+
   constructor(public navCtrl: NavController) {
-      
+      let config = new Config();
+      this.mapsUrl +=  config.googleMapsApiKey;
   }
 
 }
